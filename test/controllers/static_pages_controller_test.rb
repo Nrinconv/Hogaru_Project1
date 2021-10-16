736949dfb473dc44bcc76d0ba377409645242d10
nrinconv <nrinconv@unal.edu.co>
2021-10-16 00:46:10 -0500

    fixing deploy problems

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,12 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
   test "should get basics" do
     get static_pages_basics_url
     assert_response :success
